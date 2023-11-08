3733c32e2b2b82c988d2877e24f96a344c76db99
Cristian Ojeda <ojeda.cris.97@gmail.com>
2023-11-07 21:09:56 -0300

    Corrección de errores en js por mala interpretación, agregado de la respuesta del ejercicio 1 en el readme.md.

---
+++ README.md
@@ -0,0 +1,15 @@
+Es una calculadora simple donde se pueden ejecutar operadores basicos.
+El resultado va a ser redondeado a 2 decimales.
+
+Como observación todavia le falta agregar validaciones en los promps, estoy investigando al respecto.
+
+Con respecto al ejercicio 1.
+
+Teniendo en cuenta la siguiente estructura:
+
+function suma (a,b,callback){
+let c = a +b;
+callback();
+}
+
+El tercer parametro "callback" es utilizado cuando se necesita retornar una función o llamar otra función, normalmente se lo llama función dentro de otra función y sirve justamente para retornar valores dependiendo la logica con la que este construida.
--- js/app.js
+++ js/app.js
@@ -1,57 +1,98 @@
-const operadores = document.getElementById("operadores")
+const operadores = document.querySelectorAll("#operadores")
 
 
+for (const operador of operadores) {
+    console.log(operador.outerText);
+    operador.addEventListener("click", function () {
 
-
-operadores.addEventListener("change", function () {
-
-    const operacion = this.value
-
-    const num1 = parseFloat(document.getElementById("num1").value)
-
-    const num2 = parseFloat(document.getElementById("num2").value)
-
-    const resultado = document.getElementById("resultado")
-
-    let total
-
-
-    switch (operacion) {
+        
+        const num1 = parseFloat(document.getElementById("num1").value)
+        const num2 = parseFloat(document.getElementById("num2").value)
+        const resultado = document.getElementById("resultado")
+        let total
+        
+        
+        
+        
+       switch(operador.name) {
         case "suma":
             total = num1 + num2
-
             resultado.innerText = `
-                                El resultado es ${total.toFixed(2)}
-            `
+                                El resultado es ${total.toFixed(2)}`
             break;
+
         case "resta":
             total = num1 - num2
             resultado.innerText = `
-                                El resultado es ${total.toFixed(2)}
-            `
+                                El resultado es ${total.toFixed(2)}`
             break;
+
         case "division":
             total = num1 / num2
-
             resultado.innerText = `
-                                El resultado es ${total.toFixed(2)}
-            `
+                                El resultado es ${total.toFixed(2)}`
             break;
+
         case "multiplicacion":
             total = num1 * num2
             resultado.innerText = `
-                                El resultado es ${total.toFixed(2)}
-            `
+                                El resultado es ${total.toFixed(2)}`
             break;
 
         default:
-            resultado.innerText = `
-            Elije una operatoria!
-`
-            break;
-    }
 
-})
+            resultado.innerText = `
+            Elije una operatoria!`
+            break;
+    }}
+
+)}
+
+
+// operadores.addEventListener("click", function () {
+//     const operacion = this.value
+
+//     const num1 = parseFloat(document.getElementById("num1").value)
+//     const num2 = parseFloat(document.getElementById("num2").value)
+//     const resultado = document.getElementById("resultado")
+//     let total
+
+//     switch (operacion) {
+//         case "suma":
+//             total = num1 + num2
+
+//             resultado.innerText = `
+//                                 El resultado es ${total.toFixed(2)}
+//             `
+//             break;
+//         case "resta":
+//             total = num1 - num2
+//             resultado.innerText = `
+//                                 El resultado es ${total.toFixed(2)}
+//             `
+//             break;
+//         case "division":
+//             total = num1 / num2
+
+//             resultado.innerText = `
+//                                 El resultado es ${total.toFixed(2)}
+//             `
+//             break;
+//         case "multiplicacion":
+//             total = num1 * num2
+//             resultado.innerText = `
+//                                 El resultado es ${total.toFixed(2)}
+//             `
+//             break;
+
+//         default:
+//             resultado.innerText = `
+//             Elije una operatoria!
+// `
+//             break;
+//     }
+
+// })
 
 
 
